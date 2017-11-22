a64e95ed63371122f8dac17b540c97fb06936389
Brendon Telman <b.telman01@gmail.com>
2017-11-21 21:34:35 -0600

    add a cmd_vel subscribe and publish. remove old subscribers. Probably a good idea to write a unit test on this...

--- SabertoothSerial/src/SabertoothDriverSimple.py
+++ SabertoothSerial/src/SabertoothDriverSimple.py
@@ -38,6 +38,8 @@
 import rospy, serial, Queue
 from std_msgs.msg import String
 from SabertoothSerial.msg import SabertoothMotor
+from geometry_msgs.msg import Twist
+import tf.transformations
 
 
 class SerialMotorControl:
@@ -49,21 +51,31 @@ class SerialMotorControl:
     publishEnabled = False
     pub = 0
     queue = Queue.Queue()
+    x_min = 1
+    x_max = 1
+    r_min = 1
+    r_max = 1
 
     def constrain(self, val, min_val, max_val):
         return min(max_val, max(min_val, val))
 
-    def send_command(self, motor, power):
+    def send_command(self, x, y):
         if self.publishEnabled:
-            self.publish_raw(motor, power)
+            self.publish_raw(x, y)
         else:
-            self.motor_raw_process(motor, power)
+            x = -x  # invert x
+            V = (1 - abs(x)) * (y / 1) + y  # Calculate R+L
+            W = (1 - abs(y)) * (x / 1) + x  # Calculate R-L
+            R = (V + W) / 2  # Calculate R
+            L = (V - W) / 2  # Calculate L
+            self.motor_raw_process(0, L)
+            self.motor_raw_process(1, R)
 
     def set_publish_event(self, publish):
         self.publishEnabled = publish
         if publish:
-            rospy.init_node('motor_tester', anonymous=True)
-            self.pub = rospy.Publisher('motor_control_drive', SabertoothMotor, queue_size=10)
+            rospy.init_node('cmd_vel_motor_tester', anonymous=True)
+            self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         else:
             self.ard = serial.Serial(self.serialPort, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
 
@@ -72,11 +84,24 @@ class SerialMotorControl:
         if not self.publishEnabled:
             self.ard = serial.Serial(self.serialPort, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
 
-    def publish_raw(self, motor, power):
-        data = SabertoothMotor(motor, power)
-        data.motor = motor
-        data.power = power
-        self.pub.publish(data)
+    def publish_raw(self, y, x):
+       twist = Twist()
+       twist.linear.x = (1 - y) * (self.x_max - self.x_min) + self.x_min
+       twist.linear.y = 0
+       twist.linear.z = 0
+       twist.angular.x = 0
+       twist.angular.y = 0
+       twist.angular.z = (1 - x) * (self.r_max - self.r_min) + self.r_min
+
+       if twist.linear.x > self.x_max:
+            twist.linear.x = self.x_max
+       if twist.linear.x < self.x_min:
+            twist.linear.x = self.x_min
+       if twist.angular.z > self.r_max:
+            twist.angular.z = self.r_max
+       if twist.angular.z < self.r_min:
+            twist.angular.z = self.r_min
+       self.pub.publish(twist)
 
     def get_byte_of_motor(self, motor, power):
         power = self.constrain(power, -127, 127);
@@ -103,9 +128,12 @@ class SerialMotorControl:
     def motor_raw(self, data):
         self.ard.write(chr(data))
 
-    def drive_both(self, left_power, right_power):
-        self.send_command(0, left_power)
-        self.send_command(1, right_power)
+    def drive_both(self, x, y):
+        self.send_command(x, y)
+
+    def drive_both_raw(self, left, right):
+        self.motor_raw_process(0, left)
+        self.motor_raw_process(1, right)
 
     def drive(self, power):
         self.drive_both(power, power)
@@ -125,13 +153,22 @@ class SerialMotorControl:
     def stop(self):
         self.drive(0)
 
-    def callback(self, data):
-        self.queue.put(data)
-        rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.power)
+    def twist(self, msg):
+        rospy.loginfo("Received a /cmd_vel message!")
+        rospy.loginfo("Linear Components: [%f, %f, %f]" % (msg.linear.x, msg.linear.y, msg.linear.z))
+        rospy.loginfo("Angular Components: [%f, %f, %f]" % (msg.angular.x, msg.angular.y, msg.angular.z))
 
-    def raw_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + 'I heard raw data')
-        self.ard.write(str.decode(data))
+        # Do velocity processing here:
+        # Use the kinematics of your robot to map linear and angular velocities into motor commands
+
+        # cmd_vel.angle is the angular z component and cmd_vel.speed the linear x component
+        # https://answers.ros.org/question/209963/cmd_veltwist-transform-twist-message-into-left-and-right-motor-commands/?answer=209966#post-id-209966
+        wheel_dist = 50.8  # cm
+        speed_wish_right = (msg.angle * wheel_dist) / 2 + msg.speed
+        speed_wish_left = msg.speed * 2 - speed_wish_right
+
+        # Then set your wheel speeds (using wheel_left and wheel_right as examples)
+        self.drive_both_raw(speed_wish_left, speed_wish_right)
 
     def listener(self):
         # don't let this run unless it is a node. Better Idea; don't allow this to be a node
@@ -141,17 +178,17 @@ class SerialMotorControl:
             # anonymous=True flag means that rospy will choose a unique
             # name for our 'listener' node so that multiple listeners can
             # run simultaneously.
-            rospy.init_node('sabertooth_driver', anonymous=True)
-
-            rospy.Subscriber('motor_control_drive', SabertoothMotor, self.callback)
-            rospy.Subscriber('motor_control_drive_raw', String, self.raw_callback)
+            # rospy.init_node('sabertooth_driver', anonymous=True)
+            rospy.init_node('cmd_vel_drive_train_listener')
+            rospy.Subscriber("/cmd_vel", Twist, self.twist)
+            # rospy.Subscriber('motor_control_drive', SabertoothMotor, self.callback)
+            # rospy.Subscriber('motor_control_drive_raw', String, self.raw_callback)
             time_since_last_message = rospy.get_time()
             while not rospy.is_shutdown():
                 while not self.queue.empty():
                     time_since_last_message = rospy.get_time()
-                    data = self.queue.get()
-                    self.motor_raw_process(data.motor, data.power)
-                    data = 0
+                    cmd_vel = self.queue.get()
+                    self.twist(cmd_vel)
                 else:
                     self.timeout = self.timeout + 1
                     if rospy.get_time() - time_since_last_message > 1:
